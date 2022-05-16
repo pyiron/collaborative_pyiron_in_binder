6efce06440ee42402f2593b405701f5f319f42b8
Vincent Stock <v.stock@mpie.de>
2022-05-16 12:51:52 +0000

    Replace environment.py

--- environment.py
+++ environment.py
@@ -12,7 +12,26 @@ dependencies:
 - nglview
 - seaborn
 - pyscal
-- jupyterlab
+- jupyterlab==v3.2.2
 - pyiron-data >=0.0.5
 - sqsgenerator
 - pymatgen
+- jupyterlab-link-share
+# LSP
+- jupyterlab-lsp
+- autopep8
+- flake8>=3.8.0
+- pycodestyle>=2.6.0,<2.7.0
+- pydocstyle>=2.0.0
+- pyflakes>=2.2.0,<2.3.0
+- pylint>=2.5.0
+- yapf
+- rope>=0.10.5
+# other extensions
+- jupyterlab-git
+- jupyterlab-spellchecker
+- jupyterlab-spreadsheet-editor
+- gower==0.0.5
+- scikit-learn
+- plotnine
+- umap-learn
