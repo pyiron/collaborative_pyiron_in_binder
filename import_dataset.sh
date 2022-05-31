ac1f97c3e44ea158605098ee8cf5b4b7da2d222e
Vincent Stock <v.stock@mpie.de>
2022-05-31 10:32:21 +0000

    Update import_dataset.sh change directory to notebooks/calculation

--- import_dataset.sh
+++ import_dataset.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-for ds in $(ls calculation/*.tar.gz); do 
+for ds in $(ls notebooks/calculation/*.tar.gz); do 
     cp ${ds} .
-    cp calculation/export.csv .
+    cp notebooks/calculation/export.csv .
     file=$(basename ${ds} .tar.gz)
     echo "${file}"
     python << 'EOF'
 from pyiron_base import Project
-Project("calculation").unpack("saved_job")
+Project("notebooks/calculation").unpack("saved_job")
 EOF
     rm $(basename ${ds})
     rm export.csv
