9c7955f09010bb9aab48310cf68c74e2396bc789
tnecnivkcots <82433602+tnecnivkcots@users.noreply.github.com>
2022-06-14 16:57:47 +0200

    Update and rename import_dataset.sh to .binder/import_dataset.sh
    move to .binder with another python import project command

--- .binder/import_dataset.sh
+++ .binder/import_dataset.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-for ds in $(ls notebooks/calculation/*.tar.gz); do 
+for ds in $(ls ~/notebooks/calculation/*.tar.gz); do 
     cp ${ds} .
     cp notebooks/calculation/export.csv .
     file=$(basename ${ds} .tar.gz)
     echo "${file}"
-    python << 'EOF'
-from pyiron_base import Project
-Project("notebooks/calculation").unpack("saved_job")
-EOF
+
+    python -c "from pyiron_base import Project
+pr=Project('notebooks/calculation')
+pr.unpack('saved_job')
+"
+
     rm $(basename ${ds})
     rm export.csv
 done
+
